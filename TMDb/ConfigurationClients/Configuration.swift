eb0d9752d67450442a3a2b3115d7708979ec59b7
mourodrigo <rodrigobt20@gmail.com>
2018-02-27 20:30:41 -0300

    Created Configuration singletone class for maintain changing api paths for requests as suggested by documentation.

---
+++ TMDb/ConfigurationClients/Configuration.swift
@@ -0,0 +1,16 @@
+//
+//  Configuration.swift
+//  TMDb
+//
+//  Created by Rodrigo Bueno Tomiosso on 27/02/2018.
+//  Copyright © 2018 mourodrigo. All rights reserved.
+//
+//  Used to fetch basic app configuration and routes described on
+//  https://developers.themoviedb.org/3/configuration/get-api-configuration
+
+let baseUrl = "https://api.themoviedb.org/3/"
+let apiKey = "1f54bd990f1cdfb230adb312546d765d"
+
+class Configuration {
+
+}
