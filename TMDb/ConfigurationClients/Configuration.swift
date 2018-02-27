d15b1387865068e9627a9229d184a1972487454e
mourodrigo <rodrigobt20@gmail.com>
2018-02-27 20:43:10 -0300

    Added configuration fetch and storing from configuration API for using with on image loading.

--- TMDb/ConfigurationClients/Configuration.swift
+++ TMDb/ConfigurationClients/Configuration.swift
@@ -8,11 +8,16 @@
 //  Used to fetch basic app configuration and routes described on
 //  https://developers.themoviedb.org/3/configuration/get-api-configuration
 
+import SwiftyJSON
+import Alamofire
+
 let baseUrl = "https://api.themoviedb.org/3/"
 let apiKey = "1f54bd990f1cdfb230adb312546d765d"
 
 class Configuration {
-
+    
+    var data:JSON?
+    
     private init() {
         print("Authorization init")
     }
@@ -28,6 +33,18 @@ class Configuration {
     
     func configurationUrl() -> URL {
         return URL.init(string: baseUrl + "configuration?" + apiKeyParameter() )!
+    }
+    
+    func fetchConfiguration() {
+        print("fetching configuration ", configurationUrl())
+        Alamofire.request(configurationUrl()).responseJSON { response in
+            
+            if response.response?.statusCode == 200, let value = response.result.value {
+                self.data = JSON(value)
+                print("CONFIGURATION ", self.data)
+            }
+            NotificationCenter.default.post(name: NSNotification.Name.init("didfetchConfiguration"), object: self.data)
+        }
     }
     
 }
